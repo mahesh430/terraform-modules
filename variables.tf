3b9b90018ee458e21a4e101ba35cd350ce4eff15
Mahesh <umamahesh690@gmail.com>
2024-06-14 07:39:23 +0530

    variables names modified

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
-variable ami {
+variable "ami" {
   type        = string
   default     = ""
   description = "description"
 }
-variable instance_type {
+variable "instance_type" {
   type        = string
   default     = ""
   description = "description"
 }
-variable instance_name {
+variable "instance_name" {
   type        = string
   default     = ""
   description = "description"
